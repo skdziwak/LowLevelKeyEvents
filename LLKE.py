ef8169532770ea8cfc3e8c7294c718ebb60a6bca
skdziwak <skdziwak@gmail.com>
2021-05-22 16:40:31 +0200

    Merged macros.py with LLKE.py

--- LLKE.py
+++ LLKE.py
@@ -50,3 +50,80 @@ def keycode(k):
             if b >= fl[0] and b <= fl[1]:
                 return b - fl[0] + offset
     return getattr(LLKE_KEYS, k)
+
+if __name__ == '__main__':
+    import json
+    import argparse
+    from pathlib import Path
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str)
+    args = parser.parse_args()
+
+    config = json.loads(Path(args.config).read_text())
+
+    active = False
+
+    enabled_classes = set(config['enabled_classes'])
+
+    def get_macro(k):
+        global enabled_classes
+        for c in enabled_classes:
+            for m in config['classes'][c]:
+                if m['keycode'] == k:
+                    return m
+        return None
+
+    def a_press(action, vk):
+        m = get_macro(vk)
+        if m:
+            for vk in m['target']:
+                if action == KEY_PRESS:
+                    _press_key(ctypes.c_int(vk))
+                else:
+                    _release_key(ctypes.c_int(vk))
+            return True
+        return False
+
+    def a_shell(action, vk):
+        m = get_macro(vk)
+        if m and action == KEY_RELEASE:
+            os.system(m['target'])
+            return True
+        return False
+
+    def a_switch(action, vk):
+        m = get_macro(vk)
+        if m:
+            if action == KEY_RELEASE:
+                if m['target'] in enabled_classes:
+                    enabled_classes.remove(m['target'])
+                else:
+                    enabled_classes.add(m['target'])
+            return True
+        return False
+
+    def a_hold(action, vk):
+        m = get_macro(vk)
+        if m:
+            if action == KEY_PRESS:
+                enabled_classes.add(m['target'])
+            elif action == KEY_RELEASE:
+                enabled_classes.discard(m['target'])
+            return True
+        return False
+
+    for cl, macros in config['classes'].items():
+        for m in macros:
+            m['keycode'] = keycode(m['key'])
+            if m['action'] == 'switch':
+                set_event(m['keycode'], a_switch)
+            elif m['action'] == 'press':
+                set_event(m['keycode'], a_press)
+                m['target'] = [keycode(e) for e in m['target'].split(':')]
+            elif m['action'] == 'hold':
+                set_event(m['keycode'], a_hold)
+            elif m['action'] == 'shell':
+                set_event(m['keycode'], a_shell)
+
+    loop()
